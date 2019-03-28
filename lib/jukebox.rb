681171278c46ff07629b26799ba2c6d2d357006a
Michelle M <michellemoon236@gmail.com>
2019-03-28 02:41:41 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -73,3 +73,11 @@ end
 
 
 until answer.downcase == "exit"
+     if answer.downcase == "help"
+       help
+     elsif answer.downcase == "list"
+        list(my_songs)
+      elsif answer.downcase == "play"
+        play(my_songs)
+      else
+        puts "Please enter a command:"
