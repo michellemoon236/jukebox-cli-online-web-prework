ddb5ab0ccfcad958f02f7202ab0b04b8acf0fb95
Michelle M <michellemoon236@gmail.com>
2019-03-28 02:00:09 +0000

    Automatically backed up by Learn

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -40,6 +40,8 @@ def play(my_songs)
   #if it isn't, tell them their choice is invalid
   #if it is, play the song using the system 'open <file path>' syntax
   #get the file path of the song by looking it up in the my_songs hash
+  puts "Please enter a song name or number:"
+  response = gets.chomp
   
 end
 
